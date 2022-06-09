a751850dd12a85b547591c3db9f276b46d6aad8a
jonasty <jonas.berx@ut.ee>
2022-06-09 15:15:50 +0300

    dist plot concept

---
+++ dash/pr_wt_vis.py
@@ -0,0 +1,34 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import dash
+from re import X
+from turtle import color
+from preprocessing import transform_data
+import plotly.figure_factory as ff
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+df = pd.read_csv('./data/conform_SLA.csv')
+df.start_time = pd.to_datetime(df.start_time)
+df.end_time = pd.to_datetime(df.end_time)
+
+df = transform_data(df)
+
+
+df = df.groupby('case_variant').get_group(0)
+
+print(df)
+
+fig = px.histogram(df, x="relative_start_time",
+                   color="Activity")
+
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+# Turn off reloader if inside Jupyter  # Turn off reloader if inside Jupyter
+app.run_server(debug=True)
+ 
